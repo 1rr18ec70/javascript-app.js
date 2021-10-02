26895d583accad650ec6a4c691be25bb1d77aaa5
prashanthinim <prashanthinim2017@gmail.com>
2021-10-02 08:53:02 -0700

    dark js

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 
 var heading = document.getElementById('heading')
+var body = document.querySelector('body')
 
 console.log(heading)
 
 heading.innerHTML="i was added by you "
 heading.style="color:red;font-size:2rem"
+
+heading.classList.add('five')
+heading.classList.remove('two')
+body.classList.add('dark')
+
 console.log(heading.classList)
+
