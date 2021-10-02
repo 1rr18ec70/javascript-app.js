c0dfdc9948d4c4ddb6c81efdaa3095978a10836e
prashanthinim <prashanthinim2017@gmail.com>
2021-10-02 09:16:42 -0700

    append child

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
   var list= document.querySelector('ul')
  
   var arr=['go to college','have some knowledge','and be skilled person']
+  arr.push('complete the assignment')
 
 for (var i=0;i<arr.length;i++){
   var element= document.createElement('li')
   var textnode=document.createTextNode(arr[i])
-  console.log(element)
 element.appendChild(textnode)
-console.log(element)
+list.appendChild(element)
+
 }
