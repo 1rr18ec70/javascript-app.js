90ced9e91473d59829f042549a1379aac88263e4
prashanthinim <prashanthinim2017@gmail.com>
2021-10-02 08:42:08 -0700

    js cls one

---
+++ app.js
@@ -0,0 +1,8 @@
+
+var heading = document.getElementById('heading')
+
+console.log(heading)
+
+heading.innerHTML="i was added by you "
+heading.style="color:red;font-size:2rem"
+console.log(heading.classList)
